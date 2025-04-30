eec356c4fd85584057500d8c34dd05db9a76f5cd
missayev <feeerhd.342@gmail.com>
2025-04-30 15:50:41 +0500

    change colors of dwm

--- modules/config.h
+++ modules/config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#0f111b";
+static const char col_gray2[]       = "#686f9a";
+static const char col_gray3[]       = "#ecf0c1";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#00a3cc";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
