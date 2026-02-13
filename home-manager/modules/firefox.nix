cb9d8bd88d0bc0d933dc4093b2129dccf978c476
terow-rist <terow-rist@totatato.com>
2026-02-13 14:19:36 +0500

    update: firefox scale

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -2,7 +2,7 @@
 {
   programs.firefox.enable = true;
 
-  home.file.".mozilla/firefox/vhqtuy4m.default/user.js" =
+  home.file.".config/mozilla/firefox/5satmgyo.default/user.js" =
     lib.mkIf (host == "te14") {
       text = ''
         user_pref("layout.css.devPixelsPerPx", "1.3");
